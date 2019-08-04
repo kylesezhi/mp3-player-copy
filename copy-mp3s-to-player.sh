0335fb8d55d4da291106f5fb37ac174b9d308916
Kyle Bedell <kyle.bedell@gmail.com>
2019-08-03 22:47:00 -0400

    Initial commit

---
+++ copy-mp3s-to-player.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+mps-dir ()
+{
+  echo /Users/kyle/Downloads/mps/
+}
+
+mp3-player-dir ()
+{
+  echo '/Volumes/SPORTPLUS/Music'
+}
+
+copy-mp3s ()
+{
+  # -v is verbose
+  # -a is archive, aka, don't keep partially copied files on error
+  rsync -va $(mps-dir) $(mp3-player-dir)
+}
+
+remove-non-music-files ()
+{
+  rm "$(mp3-player-dir)/.DS_Store"
+}
+
+copy-mp3s-to-player ()
+{
+  copy-mp3s
+  remove-non-music-files
+}
+
+copy-mp3s-to-player

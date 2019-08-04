f7c4f7563cead836ff828b43d7b9a4751cfc7fdd
Kyle Bedell <kyle.bedell@gmail.com>
2019-08-04 11:04:33 -0400

    Make convert script functionalish

--- convert-m4a-to-mp3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -u
-set -o pipefail
-
-for filename in /Volumes/SPORTPLUS/Kids/*.m4a; do
-  ffmpeg "$filename.mp3" -i "$filename" -codec:a libmp3lame -qscale:a 1
-  rm "$filename"
-done
---
+++ convert-m4as-to-mp3s.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
+
+m4a-files-dir ()
+{
+  echo '/Volumes/SPORTPLUS/Kids/'
+}
+
+backup-m4a-files-dir ()
+{
+  echo "$(m4a-files-dir)/m4a-backups"
+}
+
+make-backup-dir ()
+{
+  mkdir "$(backup-m4a-files-dir)"
+}
+
+m4a-file-list ()
+{
+  echo "$(m4a-files-dir)*.mp3" # TODO mp3 -> m4a
+}
+
+move-m4a-file-to-backup-folder ()
+{
+  local fileNameAndPath="${1:?}"
+
+  local filePath
+  filePath=$(dirname "$fileNameAndPath")
+  local fileName
+  fileName=$(basename "$fileNameAndPath")
+  local backupFileNameAndPath
+  backupFileNameAndPath="$filePath/m4a-backup/$fileName"
+
+  mv '$fileNameAndPath' '$backupFileNameAndPath'
+}
+
+convert-m4as-to-mp3s ()
+{
+  #for filename in /Volumes/SPORTPLUS/Kids/*.mp3; do # TODO mp3 -> m4a
+  for filename in $(m4a-file-list); do
+    local newFileName
+    newFileName="$(basename "$filename" .m4a).mp3"
+
+    ffmpeg "$newFileName" -i "$filename" -codec:a libmp3lame -qscale:a 1
+    make-backup-dir
+    move-m4a-file-to-backup-folder "$filename"
+  done
+}
+
+convert-m4as-to-mp3s

0fe901f1d38316a7a2e74eec8545b2a4b573e188
Kyle Bedell <kyle.bedell@gmail.com>
2019-08-10 07:29:46 -0400

    Add upper limit and refactor track selection

--- index.js
+++ index.js
@@ -13,35 +13,43 @@ const location = path.resolve(userhome()
 const musicPlayerPath = '/Users/kyle/tmp'
 
 if (!fs.existsSync(musicPlayerPath)) {
+  console.error('ERROR The following path does not exist:', musicPlayerPath)
   process.exit(-1)
 }
 
-const isGoodPlayCount = (track) => track['Play Count'] > 60
-const isGoodFileType = (track) => track.Kind !== 'Protected AAC audio file'
+const isGoodPlayCount = (track) => track['Play Count'] > 3
+const isGoodFileType = (track) => !track.Kind.includes('Protected') && !track.Kind.includes('Purchased')
 const isGoodGenre = (track) => track.Genre !== 'Hip Hop/Rap'
 const isGoodArtist = (track) => track.Artist !== 'Gavin DeGraw'
 
 const isGoodMusicFile = allPass([isGoodPlayCount, isGoodFileType, isGoodGenre, isGoodArtist])
 
+let size = 0
+const sizeLimitInGb = 14
+const sizeLimitInBytes = sizeLimitInGb * 1000000000
+
+const isGoodSize = (track) => track.Size + size < sizeLimitInBytes
+
 const copyGoodMusicToPlayer = (track) => {
   try {
+    if (track.Location === null) return
     const oldLocation = decodeURI(track.Location.substr(7))
     const filename = path.basename(oldLocation)
     const newLocation = path.join(musicPlayerPath, filename)
     const isFileAbsent = !fs.existsSync(newLocation)
-    if (isGoodMusicFile(track) && isFileAbsent) {
-      console.log('Copying', filename)
-      // fs.copyFileSync(oldLocation, newLocation) // TODO tmp
+    if (isGoodMusicFile(track) && isFileAbsent && isGoodSize(track)) {
+      size = size + track.Size
+      console.log('Copying [' + filename + ']', size)
+      fs.copyFileSync(oldLocation, newLocation)
     }
   } catch (error) {
-    console.error('Could not copy', error, track)
+    console.error('ERROR Could not copy:', error, track)
   }
 }
 
 fs.createReadStream(location)
   .pipe(itunes.createTrackStream())
   .on('data', copyGoodMusicToPlayer)
-  .on('finish', () => console.log('Finished !!! :D'))
 
 // const copyCode = execSync('./copy-mp3s-to-player.sh')
 // if (copyCode < 0) {

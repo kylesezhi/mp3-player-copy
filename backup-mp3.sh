6f4901091674076123a22b8dceb46f32b450236b
Kyle Bedell <kyle.bedell@gmail.com>
2019-11-27 21:10:31 -0500

    Add mount to backup

--- backup-mp3.sh
+++ backup-mp3.sh
@@ -3,4 +3,8 @@ set -e
 set -u
 set -o pipefail
 
+sudo s3fs bedell-ramirez.music /var/music-backup -o allow_other -o use_path_request_style -o passwd_file=/home/kyle/.passwd-s3fs -o use_cache=/tmp
+
 rsync -vau /home/kyle/Downloads/mps/ /var/music-backup --delete --temp-dir=/tmp --stats --progress
+
+sudo umount /var/music-backup

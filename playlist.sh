ffc3067ea37bdd175126d4549f4c85e9cf2acbd5
Kyle Bedell <kyle.bedell@gmail.com>
2020-09-13 21:55:46 -0400

    Add playlist generator

--- playlist.sh
+++ playlist.sh
@@ -1,1 +1,7 @@
-find /home/kyle/Downloads/mps/Oliver\ Dance/ -type f > home/kyle/Downloads/mps/Playlists/Oliver\ Dance.m3u
+cd /home/kyle/Downloads/mps
+# find . -maxdepth 1 -mindepth 1 -type d -print0 | \
+#  find -type f > home/kyle/Downloads/mps/Playlists/Oliver\ Dance.m3u
+for dir in */; do
+  name=${dir%/} # remove trailing / from directories
+  find /home/kyle/Downloads/mps/"$dir" -type f > /home/kyle/Downloads/mps/Playlists/"$name".m3u
+done

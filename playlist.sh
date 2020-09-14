1d5c74272e3abd88e596890d8e205f55fd5b834c
Kyle Bedell <kyle.bedell@gmail.com>
2020-09-14 18:04:27 -0400

    Add loglines to playlist

--- playlist.sh
+++ playlist.sh
@@ -1,7 +1,13 @@
+echo ':: Removing existing playlists'
+cd /home/kyle/Downloads/mps/Playlists
+rm -rf .
+
+echo ':: Generating new playlists'
 cd /home/kyle/Downloads/mps
-# find . -maxdepth 1 -mindepth 1 -type d -print0 | \
-#  find -type f > home/kyle/Downloads/mps/Playlists/Oliver\ Dance.m3u
 for dir in */; do
   name=${dir%/} # remove trailing / from directories
-  find /home/kyle/Downloads/mps/"$dir" -type f > /home/kyle/Downloads/mps/Playlists/"$name".m3u
+  if [ "$name" != 'Playlists' ]; then
+    echo ":: Creating playlist for $name"
+    find /home/kyle/Downloads/mps/"$dir" -type f > /home/kyle/Downloads/mps/Playlists/"$name".m3u
+  fi
 done

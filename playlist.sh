d3bba79427b6894a5a4aff1803d6852f88562b7d
Kyle Bedell <kyle.bedell@gmail.com>
2020-09-14 20:45:26 -0400

    Terse loglines

--- playlist.sh
+++ playlist.sh
@@ -6,7 +6,7 @@ cd /home/kyle/Downloads/mps
 for dir in */; do
   name=${dir%/} # remove trailing / from directories
   if [ "$name" != 'Playlists' ]; then
-    echo ":: Creating playlist for $name"
+    echo ":: Creating '$name'"
     find /home/kyle/Downloads/mps/"$dir" -type f > /home/kyle/Downloads/mps/Playlists/"$name".m3u
   fi
 done
